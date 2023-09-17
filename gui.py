ee8f82030b03ae51f8c8ae0ab7b0c0e4b6af0c9c
Revlen Ganesh <revlen93@gmail.com>
2023-09-17 21:37:14 +0200

    Updated GUI

--- gui.py
+++ gui.py
@@ -14,65 +14,93 @@ class App(customtkinter.CTk):
 
         # configure window
         self.title("Clustering Reviews")
-        self.geometry(f"{1100}x{450}")
+        self.geometry(f"{1100}x{500}")
 
         # configure grid layout (4x4)
         self.grid_columnconfigure((0, 1, 2), weight=1)
-        self.grid_rowconfigure((0, 1), weight=1)
+        self.grid_rowconfigure(0, weight=1)
 
         # create the main input panel
         self.main_frame = customtkinter.CTkFrame(self, corner_radius=5)
         self.main_frame.grid_columnconfigure((0, 1), weight=1)
-        self.main_frame.grid(row=0, column=0, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.main_frame.grid(row=0, column=0, padx=(20, 10), pady=(20, 20), sticky="nsew")
 
-        self.label_path = customtkinter.CTkLabel(self.main_frame, 
+        self.input_frame = customtkinter.CTkFrame(self.main_frame, corner_radius=5, fg_color="transparent")
+        self.input_frame.grid_columnconfigure((1, 2, 3), weight=1)
+        self.input_frame.grid(row=0, column=0, columnspan=2, padx=(0, 0), pady=(0, 0), sticky="nsew")
+
+        self.label_path = customtkinter.CTkLabel(self.input_frame, 
                                               text="Enter Dataset Path/Directory: ", 
                                               font=customtkinter.CTkFont(size=13))
         self.label_path.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.entry_path = customtkinter.CTkEntry(self.main_frame, placeholder_text="c://...", width=200)
-        self.entry_path.grid(row=0, column=1, padx=10, pady=(10, 0), sticky="w")
+        self.entry_path = customtkinter.CTkEntry(self.input_frame, width=200)
+        self.entry_path.grid(row=0, column=1, columnspan=3, padx=10, pady=(10, 0), sticky="nsew")
 
-        self.label_tweet = customtkinter.CTkLabel(self.main_frame, 
-                                              text="Enter Text/Tweet Column Name: ", 
+        self.label_tweet = customtkinter.CTkLabel(self.input_frame, 
+                                              text="Enter Column Name for Reviews/Tweets: ", 
                                               font=customtkinter.CTkFont(size=13))
-        self.label_tweet.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.entry_tweet = customtkinter.CTkEntry(self.main_frame, placeholder_text="Tweets", width=200)
-        self.entry_tweet.grid(row=1, column=1, padx=10, pady=(10, 0), sticky="w")
+        self.label_tweet.grid(row=1, column=0, padx=10, pady=(15, 0), sticky="w")
+        self.entry_tweet = customtkinter.CTkEntry(self.input_frame, width=200)
+        self.entry_tweet.grid(row=1, column=1, columnspan=3, padx=10, pady=(20, 0), sticky="nsew")
+
+        self.label_sentiment = customtkinter.CTkLabel(self.input_frame, 
+                                              text="Enter Column Name for Sentiments: ", 
+                                              font=customtkinter.CTkFont(size=13))
+        self.label_sentiment.grid(row=2, column=0, padx=10, pady=(15, 0), sticky="w")
+        self.entry_sentiment = customtkinter.CTkEntry(self.input_frame, width=200)
+        self.entry_sentiment.grid(row=2, column=1, columnspan=3, padx=10, pady=(20, 10), sticky="nsew")
+
+        self.company_frame = customtkinter.CTkFrame(self.main_frame, corner_radius=5, fg_color="transparent")
+        self.company_frame.grid_columnconfigure((1, 2, 3), weight=1)
+        self.company_frame.grid(row=1, column=0, columnspan=2, padx=(0, 0), pady=(0, 0), sticky="nsew")
+
+        self.label_companyq = customtkinter.CTkLabel(self.company_frame,
+                                              text="Are all reviews in the dataset for the same company?", 
+                                              font=customtkinter.CTkFont(size=13))
+        self.label_companyq.grid(row=0, column=0, padx=10, pady=(15, 0), sticky="w")
+
+        self.radio_var = tkinter.IntVar(value=0)
+        self.radio_button_yes = customtkinter.CTkRadioButton(self.company_frame, text="Yes", variable=self.radio_var, value=0, command=self.enable_disable_input)
+        self.radio_button_yes.grid(row=0, column=1, padx=(20, 20), pady=(20, 10), sticky="n")
+        self.radio_button_no = customtkinter.CTkRadioButton(self.company_frame, text="No", variable=self.radio_var, value=1, command=self.enable_disable_input)
+        self.radio_button_no.grid(row=0, column=2, padx=(0, 20), pady=(20, 10), sticky="n")
         
-        self.label_company = customtkinter.CTkLabel(self.main_frame, 
-                                              text="Enter Company Column Name: ", 
-                                              font=customtkinter.CTkFont(size=13))
-        self.label_company.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.entry_company = customtkinter.CTkEntry(self.main_frame, placeholder_text="Leave blank", width=200)
-        self.entry_company.grid(row=2, column=1, padx=10, pady=(10, 0), sticky="w")
+        self.label_company = customtkinter.CTkLabel(self.company_frame,
+                                              text="Enter Column Name for Companies: ", 
+                                              font=customtkinter.CTkFont(size=13), text_color_disabled="#808080")
+        self.label_company.grid(row=1, column=0, padx=10, pady=(20, 0), sticky="w")
+        self.entry_company = customtkinter.CTkEntry(self.company_frame, placeholder_text="")
+        self.entry_company.grid(row=1, column=1, columnspan=3, padx=10, pady=(20, 0), sticky="nsew")
 
-        self.label_sentiment = customtkinter.CTkLabel(self.main_frame, 
-                                              text="Enter Sentiment Column Name: ", 
-                                              font=customtkinter.CTkFont(size=13))
-        self.label_sentiment.grid(row=3, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.entry_sentiment = customtkinter.CTkEntry(self.main_frame, placeholder_text="Sentiment", width=200)
-        self.entry_sentiment.grid(row=3, column=1, padx=10, pady=(10, 0), sticky="w")
+        self.label_company_name = customtkinter.CTkLabel(self.company_frame,
+                                              text="Enter the Name of the Company for Review Clustering: ", 
+                                              font=customtkinter.CTkFont(size=13), text_color_disabled="#808080")
+        self.label_company_name.grid(row=2, column=0, padx=10, pady=(20, 0), sticky="w")
+        self.entry_company_name = customtkinter.CTkEntry(self.company_frame, placeholder_text="")
+        self.entry_company_name.grid(row=2, column=1, columnspan=3, padx=10, pady=(20, 0), sticky="nsew")
+
 
         self.radio_group_frame = customtkinter.CTkFrame(self.main_frame, corner_radius=5)
+        self.radio_group_frame.grid_columnconfigure((0, 1, 2), weight=1)
         self.radio_group_frame.grid(row=4, column=0, columnspan=2, padx=(20, 20), pady=(20, 10), sticky="nsew")
-        self.radio_var = tkinter.IntVar(value=0)
+        self.radio_cluster_var = tkinter.IntVar(value=0)
         self.label_radio_group = customtkinter.CTkLabel(self.radio_group_frame, text="Clustering Method:", font=customtkinter.CTkFont(size=15, weight="bold"))
         self.label_radio_group.grid(row=0, column=1, padx=(20, 20), pady=(5, 5), sticky="nsew")
-        self.radio_button_k_means = customtkinter.CTkRadioButton(self.radio_group_frame, text="K-Means Clustering", variable=self.radio_var, value=0)
+        self.radio_button_k_means = customtkinter.CTkRadioButton(self.radio_group_frame, text="K-Means Clustering", variable=self.radio_cluster_var, value=0)
         self.radio_button_k_means.grid(row=1, column=0, padx=(20, 20), pady=(5, 10), sticky="n")
-        self.radio_button_hierarchical = customtkinter.CTkRadioButton(self.radio_group_frame, text="Heirarchical Clustering", variable=self.radio_var, value=1)
+        self.radio_button_hierarchical = customtkinter.CTkRadioButton(self.radio_group_frame, text="Heirarchical Clustering", variable=self.radio_cluster_var, value=1)
         self.radio_button_hierarchical.grid(row=1, column=1, padx=(20, 20), pady=(5, 10), sticky="n")
-        self.radio_button_dbscan = customtkinter.CTkRadioButton(self.radio_group_frame, text="DBSCAN Clustering", variable=self.radio_var, value=2)
+        self.radio_button_dbscan = customtkinter.CTkRadioButton(self.radio_group_frame, text="DBSCAN Clustering", variable=self.radio_cluster_var, value=2)
         self.radio_button_dbscan.grid(row=1, column=2, padx=(20, 20), pady=(5, 10), sticky="n")
 
-        self.main_button = customtkinter.CTkButton(self.main_frame, text="Load Dataset", border_width=2, width=410, command=self.main_button_event)
-        self.main_button.grid(row=5, column=0, columnspan=3, padx=10, pady=(20, 20), sticky="w")
+        self.main_button = customtkinter.CTkButton(self.main_frame, text="Load Dataset and Perform Clustering", border_width=2, width=410, command=self.main_button_event)
+        self.main_button.grid(row=5, column=0, columnspan=3, padx=10, pady=(20, 20), sticky="nsew")
 
         #output_frame
         self.likes_frame = customtkinter.CTkFrame(self, corner_radius=5)
         self.likes_frame.grid_columnconfigure(0, weight=1)
         self.likes_frame.grid_rowconfigure(1, weight=1)
-        self.likes_frame.grid(row=0, column=1, padx=(20, 20), pady=(20,0), sticky="nsew")
+        self.likes_frame.grid(row=0, column=1, padx=(10, 10), pady=(20,20), sticky="nsew")
         self.label_likes = customtkinter.CTkLabel(self.likes_frame, text="Likes:", font=customtkinter.CTkFont(size=15, weight="bold"))
         self.label_likes.grid(row=0, column=0, padx=(20, 20), pady=(5, 5), sticky="w")
         self.likes_textbox = customtkinter.CTkTextbox(self.likes_frame, width=175, height=300)
@@ -81,18 +109,26 @@ class App(customtkinter.CTk):
         self.dislikes_frame = customtkinter.CTkFrame(self, corner_radius=5)
         self.dislikes_frame.grid_columnconfigure(0, weight=1)
         self.dislikes_frame.grid_rowconfigure(1, weight=1)
-        self.dislikes_frame.grid(row=0, column=2, padx=(20, 20), pady=(20,0), sticky="nsew")
+        self.dislikes_frame.grid(row=0, column=2, padx=(10, 20), pady=(20,20), sticky="nsew")
         self.label_dislikes = customtkinter.CTkLabel(self.dislikes_frame, text="Dislikes:", font=customtkinter.CTkFont(size=15, weight="bold"))
         self.label_dislikes.grid(row=0, column=0, padx=(20, 20), pady=(5, 5), sticky="w")
         self.dislikes_textbox = customtkinter.CTkTextbox(self.dislikes_frame, width=175, height=300)
         self.dislikes_textbox.grid(row=1, column=0, padx=(10, 10), pady=(0, 10), sticky="nsew")
+
+        # set default values
+        self.entry_company_name.configure(state="disabled")
+        self.entry_company.configure(state="disabled")
+        self.label_company_name.configure(state="disabled")
+        self.label_company.configure(state="disabled")
+        self.likes_textbox.configure(state="disabled")
+        self.dislikes_textbox.configure(state="disabled")
 
     def main_button_event(self):
         path = self.entry_path.get()
         tweet = self.entry_tweet.get()
         sentiment = self.entry_sentiment.get()
         company = self.entry_company.get()
-        clust_method = self.radio_var.get()
+        clust_method = self.radio_cluster_var.get()
 
         df, pos_df, neg_df = implementation.preprocessing(path, tweet, sentiment, company)
         print(df.head())
@@ -118,9 +154,19 @@ class App(customtkinter.CTk):
         self.dislikes_textbox.insert("0.0",neg_string)
 
 
-    def open_input_dialog_event(self):
-        dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
-        print("CTkInputDialog:", dialog.get_input())
+    def enable_disable_input(self):
+        if self.radio_var.get() == 1:
+            self.entry_company.configure(state="normal")
+            self.entry_company_name.configure(state="normal")
+            self.label_company.configure(state="normal")
+            self.label_company_name.configure(state="normal")
+        else:
+            self.entry_company.delete(0, customtkinter.END)
+            self.entry_company_name.delete(0, customtkinter.END)
+            self.entry_company.configure(state="disabled")
+            self.entry_company_name.configure(state="disabled")
+            self.label_company.configure(state="disabled")
+            self.label_company_name.configure(state="disabled")
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
@@ -128,9 +174,6 @@ class App(customtkinter.CTk):
     def change_scaling_event(self, new_scaling: str):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
-
-    def sidebar_button_event(self):
-        print("sidebar_button click")
 
 
 if __name__ == "__main__":
